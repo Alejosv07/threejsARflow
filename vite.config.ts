305bfd888eb68db0bc2b41ee53582a277c2c3479
Alejosv07 <balrking07@gmail.com>
2025-11-02 19:03:44 -0600

    conf

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // **Esta línea es CRUCIAL para GitHub Pages.**
+  // Debe ser el nombre exacto de tu repositorio, precedido y seguido de un slash.
+  base: '/threejsARflow/', 
+  
+  plugins: [],
+});
